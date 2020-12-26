b8f3a2e595f5de978b42fdf7f18f9877e7bcd244
carlos.eduardo <ceamattos.jf@gmail.com>
2020-12-26 15:06:36 -0300

    more styles

--- src/pages/Repository/index.tsx
+++ src/pages/Repository/index.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
-import { useRouteMatch } from 'react-router-dom';
+import { useRouteMatch, Link } from 'react-router-dom';
+import { Header, RepositoryInfo, Issues } from './styles';
+import logoImg from '../../assets/logo.svg';
+import { FiChevronsLeft, FiChevronRight } from 'react-icons/fi';
 
 interface RepositoryParams {
   repository: string;
@@ -8,7 +11,52 @@ interface RepositoryParams {
 const Repository: React.FC = () => {
   const { params } = useRouteMatch<RepositoryParams>();
 
-  return <h1>Repository {params.repository}</h1>;
+  return (
+    <>
+      <Header>
+        <img src={logoImg} alt="Github Explorer" />
+        <Link to="/">
+          <FiChevronsLeft size={16} />
+          back
+        </Link>
+      </Header>
+      <RepositoryInfo>
+        <header>
+          <img
+            src="https://avatars0.githubusercontent.com/u/28929274?s=200&v=4"
+            alt="Rocketseat"
+          />
+          <div>
+            <strong>Rocketseat/unform</strong>
+            <p>descrição do repositorio</p>
+          </div>
+        </header>
+        <ul>
+          <li>
+            <strong>1808</strong>
+            <span>Stars</span>
+          </li>
+          <li>
+            <strong>48</strong>
+            <span>Forks</span>
+          </li>
+          <li>
+            <strong>67</strong>
+            <span>Opened issues</span>
+          </li>
+        </ul>
+      </RepositoryInfo>
+      <Issues>
+        <Link to={'blablabla'}>
+          <div>
+            <strong>repository.full_name</strong>
+            <p>repository.description</p>
+          </div>
+          <FiChevronRight size={20} />
+        </Link>
+      </Issues>
+    </>
+  );
 };
 
 export default Repository;
